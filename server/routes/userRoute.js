3b159e102596441c92d5011a98a4d35aa2e2db22
mdwahid <mdw30242@gmail.com>
2025-09-04 18:09:02 +0530

    backend

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { getUserData, purchaseCourse, userEnrolledCourese } from '../controllers/userController.js';
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourese } from '../controllers/userController.js';
 
 const userRouter=express.Router();
 
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourese)
 userRouter.post('/purchase', purchaseCourse)
+
+userRouter.post('/update-course-progress', updateUserCourseProgress)
+userRouter.post('/get-course-progress', getUserCourseProgress)
+userRouter.post('/add-rating', addUserRating)
 
 
 export default userRouter;
